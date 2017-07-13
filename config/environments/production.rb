5487e443d969822d0d861c8e238cdace874d6fb3
Reza <neelamyuvasoft147.com>
2017-07-13 18:48:33 +0530

    #feture changes in path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://whispering-eyrie-49738.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://socialio.herokuapp.com' }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
